e0587547031486a27e1726506a368f7d49d4c396
Danniel Sousa <danniel.sousa@gmail.com>
2021-11-18 00:09:57 -0300

    Alterações

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,8 +1,25 @@
 const { ipcRenderer } = require('electron');
+const timer = require('./timer');
+
+let imgs = [ 'img/play-button.svg', 'img/stop-button.svg'];
 
 let linkSobre = document.querySelector('#link-sobre');
-
+let botaoPlay = document.querySelector('.botao-play');
+let tempo = document.querySelector('.tempo');
 
 linkSobre.addEventListener('click' , function(){
     ipcRenderer.send('abrir-janela-sobre');
 });
+let play = false;
+
+botaoPlay.addEventListener('click', () => {
+    if (play) {
+        timer.parar();
+        play = false;
+    } else {
+        timer.iniciar(tempo);
+        play = true;
+    }
+    imgs = imgs.reverse();
+    botaoPlay.src = imgs[0];
+})
